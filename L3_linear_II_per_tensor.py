e20bb8b49c40636d1c17987e2648ed04f51782f8
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-09-15 11:08:52 +0300

    Python file L3_linear_II_per_tensor.py added.

---
+++ L3_linear_II_per_tensor.py
@@ -0,0 +1,24 @@
+import torch
+
+from helper import linear_q_symmetric, get_q_scale_symmetric, linear_dequantization
+from helper import plot_quantization_errors, quantization_error
+
+
+## ------------------------------------------------------##
+test_tensor = torch.tensor(
+                        [[191.6, -13.5, 728.6],
+                         [92.14, 295.5,  -184],
+                         [0,     684.6, 245.5]]
+                        )
+
+## ------------------------------------------------------##
+quantized_tensor, scale = linear_q_symmetric(test_tensor)
+
+## ------------------------------------------------------##
+dequantized_tensor = linear_dequantization(quantized_tensor, scale, 0)
+
+## ------------------------------------------------------##
+plot_quantization_errors(test_tensor, quantized_tensor, dequantized_tensor)
+
+## ------------------------------------------------------##
+print(f"""Quantization Error : {quantization_error(test_tensor, dequantized_tensor)}""")
